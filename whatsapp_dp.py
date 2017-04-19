ea27699d80fdfaeb83af6ae15b865f1437ab4e2d
Piyush Mishra <piyush11mishra@gmail.com>
2017-04-19 19:16:19 +0530

    Added gitignore, modified the exception and other cleanups

--- whatsapp_dp.py
+++ whatsapp_dp.py
@@ -1,29 +1,30 @@
 #! /usr/bin/python3
 from PIL import Image
-import sys
+import sys, os
 
 isLandscape = lambda x: max(x)==x[0]
 
 def getImageFromWeb(url):
     '''
     '''
-    
+
 def getImageFromDisk(file):
     try:
         im = Image.open(file)
         print("Opened image {}".format(file))
         return process(im, file)
-    except:
+    except IOError:
         return "Error opening image..."
-    
-    
+
+
 def process(im, file="output.jpg"):
     if not isLandscape(im.size):
         print("Please provide landscape image. Only they are supported as of now")
     else:
         new_im = Image.new(im.mode, (max(im.size), max(im.size)), color=0)
         new_im.paste(im, box=(0, (im.size[0]-im.size[1])//2))
-        outfile = '{}_i.jpg'.format(file[:-4])
+        (filename, fileExtension) = os.path.splitext(file)
+        outfile = '{fname}_i{fext}'.format(fname=filename, fext=fileExtension)
         new_im.save(outfile)
         return "Image saved as {}".format(outfile)
 
@@ -33,5 +34,5 @@ if __name__ == '__main__':
     else:
         res = getImageFromDisk(sys.argv[1])
         print(res)
-    
-    
+
+
